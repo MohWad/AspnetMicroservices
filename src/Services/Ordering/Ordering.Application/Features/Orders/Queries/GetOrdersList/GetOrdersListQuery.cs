f43595ce650367ac4749cc1e168260677da88504
Mohammed Alawad <mohawad_53@hotmail.com>
2022-02-25 13:00:40 +0300

    completed development of the application layer of ordering API

---
+++ src/Services/Ordering/Ordering.Application/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+
+namespace Ordering.Application.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQuery : IRequest<List<OrdersVm>>
+    {
+        public GetOrdersListQuery(string username)
+        {
+            Username = username ?? throw new ArgumentNullException(nameof(username));
+        }
+
+        public string Username { get; set; }
+    }
+}
